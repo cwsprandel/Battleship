18c1e8fe89d19d06bd83f64aab432f8d633cd979
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-07-17 19:23:34 -0500

    Ship Length Tests

---
+++ script.js
@@ -0,0 +1,28 @@
+//carrier: 5
+//battleship: 4
+//cruiser: 3
+//submarine: 3
+//destroyer: 2
+
+const shipFactory = (shipName) => {
+    switch (shipName) {
+        case 'carrier':
+            length = 5
+            break
+        case 'battleship':
+            length = 4
+            break
+        case 'cruiser':
+            length = 3
+            break
+        case 'submarine':
+            length = 3
+            break
+        case 'destroyer':
+            length = 2
+    }
+
+    return {shipName, length}
+}
+
+module.exports = shipFactory
--- script.test.js
+++ script.test.js
@@ -2,11 +2,20 @@ const shipFactory = require('./script')
 
 
 describe('ship factory tests', () => {
+    let testShipCarrier
     beforeAll(() => {
-        let testShipCarrier = shipFactory('carrier')
+        testShipCarrier = shipFactory('carrier')
+        testShipSub = shipFactory('submarine')
     })
 
-    test('ship has length', () => {
+    test('ship has length (test 1)', () => {
+        expect(testShipCarrier.length).toBe(5)
+    })
+    test('ship has length (test 2)', () => {
+        expect(testShipSub.length).toBe(3)
+    })
+
+    test('ship ', () => {
         expect(testShipCarrier.length).toBe(5)
     })
 })
